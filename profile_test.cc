5f0c569d95b361cf5d78a158665a16c71a652e59
jishnusen <jishnu1@gmail.com>
2019-05-21 13:37:56 -0700

    Add backwards test

--- profile_test.cc
+++ profile_test.cc
@@ -32,3 +32,31 @@ TEST(Profile, Test) {
   EXPECT_NEAR(profile.GetPoint(t - kEpsilon).position, goal.position, kEpsilon * 2);
   EXPECT_NEAR(profile.GetPoint(t - kEpsilon).velocity, goal.velocity, kEpsilon * 2);
 }
+
+TEST(Profile, TestBackwards) {
+  // params
+  const Profile::ProfilePoint current = {1., 0.};
+  const Profile::ProfilePoint goal = {0., 0.};
+
+  Profile profile(current);
+  profile.SetGoal(goal);
+
+  double t = profile.GetTime();
+
+  Profile::ProfilePoint prev_point;
+  Profile::ProfilePoint point = profile.GetPoint(0.0);
+
+  std::ofstream csv("/tmp/profile_backwards.csv");
+
+  for (double i = 0; i < t; i += kDt) {
+    prev_point = point;
+    point = profile.GetPoint(i);
+    EXPECT_LE(point.velocity, kMaxVelocity);
+    EXPECT_LE(point.velocity - prev_point.velocity, kMaxAcceleration * kDt + kEpsilon);
+
+    csv << i << "," << point.position << "," << point.velocity << "\n";
+  }
+
+  EXPECT_NEAR(profile.GetPoint(t - kEpsilon).position, goal.position, kEpsilon * 2);
+  EXPECT_NEAR(profile.GetPoint(t - kEpsilon).velocity, goal.velocity, kEpsilon * 2);
+}
