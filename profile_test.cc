a293e4948e0caed2d11c621b6a0cf138cf081270
jishnusen <jishnu1@gmail.com>
2019-05-21 13:32:06 -0700

    Clean up test

--- profile_test.cc
+++ profile_test.cc
@@ -2,9 +2,17 @@
 #include "profile.h"
 #include <fstream>
 
+constexpr double kDt = 0.005;
+const double kEpsilon = 1e-9;
+
 TEST(Profile, Test) {
-  Profile profile({0., 0.});
-  profile.SetGoal({1., 0.});
+  // params
+  const Profile::ProfilePoint current = {0., 0.};
+  const Profile::ProfilePoint goal = {2., 0.};
+
+  Profile profile(current);
+  profile.SetGoal(goal);
+
   double t = profile.GetTime();
 
   Profile::ProfilePoint prev_point;
@@ -12,15 +20,15 @@ TEST(Profile, Test) {
 
   std::ofstream csv("/tmp/profile.csv");
 
-  for (double i = 0; i < t; i += 0.005) {
+  for (double i = 0; i < t; i += kDt) {
     prev_point = point;
     point = profile.GetPoint(i);
     EXPECT_LE(point.velocity, kMaxVelocity);
-    EXPECT_LE(point.velocity - prev_point.velocity, kMaxAcceleration * 0.005 + 1e-9);
+    EXPECT_LE(point.velocity - prev_point.velocity, kMaxAcceleration * kDt + kEpsilon);
 
     csv << i << "," << point.position << "," << point.velocity << "\n";
   }
 
-  EXPECT_NEAR(profile.GetPoint(t).position, 1., 1e-9);
-  EXPECT_NEAR(profile.GetPoint(t).velocity, 0., 1e-9);
+  EXPECT_NEAR(profile.GetPoint(t).position, goal.position, kEpsilon);
+  EXPECT_NEAR(profile.GetPoint(t).velocity, goal.velocity, kEpsilon);
 }
