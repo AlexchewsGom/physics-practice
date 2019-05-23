5d8c619666f964f9696fec527866d805257de6e9
jishnusen <jishnu1@gmail.com>
2019-05-22 19:20:40 -0700

    Check if backwards and theoretically invert profile

--- profile.h
+++ profile.h
@@ -12,11 +12,16 @@ public:
     double position;
     double velocity;
   };
-  Profile(ProfilePoint current) {
-    current_ = current;
-  }
-  void SetGoal(ProfilePoint goal) { 
-    goal_ = goal; 
+  Profile(ProfilePoint current) { current_ = current; }
+  void SetGoal(ProfilePoint goal) {
+    double delta_v = goal.velocity - current_.velocity;
+    double delta_s = goal.position - current_.position;
+    double delta_t = std::abs(delta_v) / kMaxAcceleration;
+    backwards_ = delta_t * (delta_v / 2 + current_.velocity) > delta_s;
+    goal_ = backwards_ ? ProfilePoint({-goal.position, -goal.velocity}) : goal;
+    current_ = backwards_
+                   ? ProfilePoint({-current_.position, -current_.velocity})
+                   : current_;
     CalculateTime();
   }
   const double GetTime() { return end_deccel_t_; }
@@ -31,6 +36,8 @@ private:
   double end_accel_t_;
   double end_const_t_;
   double end_deccel_t_;
+
+  bool backwards_;
 };
 
 #endif // PROFILE_H_
